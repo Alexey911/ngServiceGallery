709f9dc0ae1383279845c3b85055662a07942f91
Alexey Zhytnik <alexeyzhytnik@gmail.com>
2017-04-15 16:01:27 +0300

    add AES support

---
+++ app/services/crypto.service.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('cryptService', cryptService);
+
+    cryptService.inject = [];
+
+    function cryptService() {
+
+        return {
+            encrypt: encrypt,
+            decrypt: decrypt
+        };
+
+        function encrypt(message, key) {
+            return CryptoJS.AES.encrypt(message, key).toString();
+        }
+
+        function decrypt(message, key) {
+            return CryptoJS.AES.decrypt(message, key).toString(CryptoJS.enc.Utf8);
+        }
+
+    }
+})();
