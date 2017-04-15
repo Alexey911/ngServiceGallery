7c922c1513440171415a689b250d7ac5eb1a3b6a
Alexey Zhytnik <alexeyzhytnik@gmail.com>
2017-04-15 18:57:55 +0300

    testing support

---
+++ karma.conf.js
@@ -0,0 +1,31 @@
+module.exports = function(config) {
+  config.set({
+
+    basePath: './src',
+
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js'
+      /*TODO: ADD TESTING FOLDERS*/
+    ],
+
+    autoWatch: true,
+
+    frameworks: ['jasmine'],
+
+    browsers: ['Chrome'],
+
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-jasmine',
+      'karma-junit-reporter'
+    ],
+
+    junitReporter: {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
