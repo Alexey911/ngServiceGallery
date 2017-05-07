74640c195ec9c06c8d27345d54e9a4227a1c65ab
Alexey Zhytnik <alexeyzhytnik@gmail.com>
2017-05-07 10:59:38 +0300

    transfer scheduler to common module

--- src/modules/shared/services/scheduler.service.js
+++ src/modules/shared/services/scheduler.service.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('ngServiceGallery.monitoring')
+        .module('ngServiceGallery.common')
         .factory('scheduler', scheduler);
 
     scheduler.$inject = ['$log', '$interval'];
