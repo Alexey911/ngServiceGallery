9fe1dc0f06073e5d349cd2e28b454d34453e6c05
Alexey Zhytnik <alexeyzhytnik@gmail.com>
2017-05-08 10:55:33 +0300

    add request builder

--- src/modules/crud/services/crud.service.js
+++ src/modules/crud/services/crud.service.js
@@ -5,9 +5,9 @@
         .module('ngServiceGallery.crud')
         .factory('crudService', crudService);
 
-    crudService.$inject = ['$http', '$httpParamSerializerJQLike'];
+    crudService.$inject = ['$http', 'requestBuilder'];
 
-    function crudService($http, $httpParamSerializerJQLike) {
+    function crudService($http, requestBuilder) {
 
         return {
             send: send,
@@ -31,45 +31,9 @@
             return ['application/x-www-form-urlencoded', 'multipart/form-data'];
         }
 
-        function send(request) {
-            const req = {
-                url: request.url,
-                method: request.method,
-                headers: transformToFields(request.headers),
-                params: transformToFields(request.params)
-            };
-
-            if (request.body) setUpBody(req, request.body);
-
-            $http(req);
-        }
-
-        function setUpBody(request, body) {
-            if (body.contentType === 'multipart/form-data') {
-                request.headers['Content-Type'] = undefined;
-                request.data = transformToForm(body.pairs);
-            } else {
-                request.headers['Content-Type'] = body.contentType;
-                request.data = $httpParamSerializerJQLike(transformToFields(body.pairs));
-            }
-        }
-
-        function transformToFields(items) {
-            const result = {};
-
-            for (let item of items) {
-                result[item.name] = item.value;
-            }
-            return result;
-        }
-
-        function transformToForm(items) {
-            let form = new FormData();
-
-            for (let item of items) {
-                form.append(item.name, item.value);
-            }
-            return form;
+        function send(data) {
+            const request = requestBuilder.build(data);
+            $http(request);
         }
     }
 })();
